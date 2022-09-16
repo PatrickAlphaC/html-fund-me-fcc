4fd01d99f7e421ec7c55fa5b64763b108aae691e
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-09-17 02:16:22 +0500

    Added try catch in listenForTransactionMine
    Added reject() by adding try catch in the Promise!

--- index.js
+++ index.js
@@ -79,13 +79,17 @@ async function getBalance() {
 }
 
 function listenForTransactionMine(transactionResponse, provider) {
-  console.log(`Mining ${transactionResponse.hash}`)
-  return new Promise((resolve, reject) => {
-    provider.once(transactionResponse.hash, (transactionReceipt) => {
-      console.log(
-        `Completed with ${transactionReceipt.confirmations} confirmations. `
-      )
-      resolve()
+    console.log(`Mining ${transactionResponse.hash}`)
+    return new Promise((resolve, reject) => {
+        try {
+            provider.once(transactionResponse.hash, (transactionReceipt) => {
+                console.log(
+                    `Completed with ${transactionReceipt.confirmations} confirmations. `
+                )
+                resolve()
+            })
+        } catch (error) {
+            reject(error)
+        }
     })
-  })
 }
