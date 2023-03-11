ae474d91b6352ad7c643a1e84e1b625c8acd4f4e
Ali Murtaza <alimurtaza-assistant@outlook.com>
2023-03-11 16:56:40 +0500

    Update constants.js file - Sepolia Update
    Update the contract address to deployed contract on sepolia

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const contractAddress = "0x87D29af343dBa7c8AAAD4F53fC2a17B52b1895ca"
 export const abi = [
   {
     inputs: [
