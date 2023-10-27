cc7df461837454f6dbdf1020cca020826eab32cb
Sarthak <sarthakrawatbiz@gmail.com>
2023-10-27 11:29:36 +0530

    Adding axios CDN and using CoinGecko API to use amount in USD instead of ETH amount

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
 import { abi, contractAddress } from "./constants.js"
+const axios = window.axios
 
 const connectButton = document.getElementById("connectButton")
 const withdrawButton = document.getElementById("withdrawButton")
@@ -11,71 +12,86 @@ fundButton.onclick = fund
 balanceButton.onclick = getBalance
 
 async function connect() {
-  if (typeof window.ethereum !== "undefined") {
-    try {
-      await ethereum.request({ method: "eth_requestAccounts" })
-    } catch (error) {
-      console.log(error)
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+        connectButton.innerHTML = "Connected"
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+        console.log(accounts)
+    } else {
+        connectButton.innerHTML = "Please install MetaMask"
     }
-    connectButton.innerHTML = "Connected"
-    const accounts = await ethereum.request({ method: "eth_accounts" })
-    console.log(accounts)
-  } else {
-    connectButton.innerHTML = "Please install MetaMask"
-  }
 }
 
 async function withdraw() {
-  console.log(`Withdrawing...`)
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    await provider.send('eth_requestAccounts', [])
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(contractAddress, abi, signer)
-    try {
-      const transactionResponse = await contract.withdraw()
-      await listenForTransactionMine(transactionResponse, provider)
-      // await transactionResponse.wait(1)
-    } catch (error) {
-      console.log(error)
+    console.log(`Withdrawing...`)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        await provider.send("eth_requestAccounts", [])
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+            // await transactionResponse.wait(1)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        withdrawButton.innerHTML = "Please install MetaMask"
     }
-  } else {
-    withdrawButton.innerHTML = "Please install MetaMask"
-  }
 }
 
 async function fund() {
-  const ethAmount = document.getElementById("ethAmount").value
-  console.log(`Funding with ${ethAmount}...`)
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(contractAddress, abi, signer)
-    try {
-      const transactionResponse = await contract.fund({
-        value: ethers.utils.parseEther(ethAmount),
-      })
-      await listenForTransactionMine(transactionResponse, provider)
-    } catch (error) {
-      console.log(error)
+    const usdAmount = parseFloat(document.getElementById("fund-amount").value)
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            // Fetch the ETH price from CoinGecko API
+            const response = await axios.get(
+                "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+            )
+            if (response.data && response.data.ethereum && response.data.ethereum.usd) {
+                const ethPriceInUSD = response.data.ethereum.usd
+                const ethAmount = (usdAmount / ethPriceInUSD).toString()
+                try {
+                    const tx_response = await contract.fund({
+                        value: ethers.utils.parseEther(ethAmount),
+                    })
+                    await listenForTransactionMined(tx_response, provider)
+                    console.log("Done!")
+                } catch (error) {
+                    console.log("Transaction error:", error)
+                }
+            } else {
+                console.log("Failed to fetch the ETH amount.")
+            }
+        } catch (error) {
+            console.log("API request error:", error)
+        }
+    } else {
+        fundButton.innerHTML = "Please install MetaMask"
     }
-  } else {
-    fundButton.innerHTML = "Please install MetaMask"
-  }
 }
 
 async function getBalance() {
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    try {
-      const balance = await provider.getBalance(contractAddress)
-      console.log(ethers.utils.formatEther(balance))
-    } catch (error) {
-      console.log(error)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        try {
+            const balance = await provider.getBalance(contractAddress)
+            console.log(ethers.utils.formatEther(balance))
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        balanceButton.innerHTML = "Please install MetaMask"
     }
-  } else {
-    balanceButton.innerHTML = "Please install MetaMask"
-  }
 }
 
 function listenForTransactionMine(transactionResponse, provider) {
@@ -83,9 +99,7 @@ function listenForTransactionMine(transactionResponse, provider) {
     return new Promise((resolve, reject) => {
         try {
             provider.once(transactionResponse.hash, (transactionReceipt) => {
-                console.log(
-                    `Completed with ${transactionReceipt.confirmations} confirmations. `
-                )
+                console.log(`Completed with ${transactionReceipt.confirmations} confirmations. `)
                 resolve()
             })
         } catch (error) {
